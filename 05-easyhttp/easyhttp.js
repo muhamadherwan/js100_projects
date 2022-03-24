576c837345ed95f3d2f218392f4d90da193566d1
muhamadherwan <hello.wanlab@gmail.com>
2022-03-24 22:25:14 +0800

    create and update post

--- 05-easyhttp/easyhttp.js
+++ 05-easyhttp/easyhttp.js
@@ -26,8 +26,50 @@ easyHTTP.prototype.get = function(url, callback) {
     this.http.send();
 }
 
-// Make HTTP POST Request 
+// Make HTTP POST Request
+easyHTTP.prototype.post = function(url, data, callback) {
+    // request to sent the new post
+    this.http.open('POST', url, true);
+    // request the header
+    this.http.setRequestHeader('Content-type', 'application/json');
+    
+    let self = this;
+    // call back to return the create new post
+    this.http.onload = function() {
+        if (self.http.status === 201) {
+            callback(null, self.http.responseText);
+        } else {
+            callback('error: ' + self.http.status); 
+        }
+    }
+
+    // send the new post
+    this.http.send(JSON.stringify(data));
+}
+
 
 // Make HTTP PUT Request
+easyHTTP.prototype.put = function(url, data, callback){
+    // request to edit the post
+    this.http.open('PUT', url, true);
+    // request header
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    // callback to return the edit post result
+    let self = this;
+    // callback the request result
+    this.http.onload = function() {
+        if (self.http.status === 200) {
+            callback(null, self.http.responseText);
+        } else {
+            callback('error: ' + self.http.status);
+        }
+    }
+
+    
+    // edit the post
+    this.http.send(JSON.stringify(data));
+}
+
 
 // Make HTTP DELETE Request
--- 05-easyhttp/script.js
+++ 05-easyhttp/script.js
@@ -11,12 +11,35 @@ const http = new easyHTTP;
 
 
 // get single post by post id
-http.get('https://jsonplaceholder.typicode.com/posts/2', function(err, posts) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(posts);
-    }
-});
+// http.get('https://jsonplaceholder.typicode.com/posts/2', function(err, posts) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+// });
 
-// create new post
+// create new data
+const data =  {
+    title: 'Custom Post puspus fafa',
+    body: 'this is a custom post wuswus' 
+};
+
+// // create new post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// edit post
+// http.put('https://jsonplaceholder.typicode.com/posts/2', data, function(err, post){
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
