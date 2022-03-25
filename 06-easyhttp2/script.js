4b7878b5035b64cc801d6c43beec181dff05f469
muhamadherwan <hello.wanlab@gmail.com>
2022-03-25 18:37:58 +0800

    post request

--- 06-easyhttp2/script.js
+++ 06-easyhttp2/script.js
@@ -2,6 +2,18 @@
 const http = new EasyHTTP();
 
 // Get Users
-http.get('http://jsonplaceholder.typicode.com/users')
+// http.get('http://jsonplaceholder.typicode.com/users')
+// .then(data => console.log(data))
+// .catch(err => console.log(err));
+
+// new user data
+const data = {
+    name: 'Chino',
+    username: 'deftones',
+    email: 'chino@deftones.com'
+}
+
+// Post new user
+http.post('http://jsonplaceholder.typicode.com/users', data)
 .then(data => console.log(data))
 .catch(err => console.log(err));
