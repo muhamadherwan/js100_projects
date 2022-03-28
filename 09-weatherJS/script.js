5053769e0f93e41a0feeebe23d2c4aef0ed993ce
muhamadherwan <hello.wanlab@gmail.com>
2022-03-28 12:37:38 +0800

    change city event

--- 09-weatherJS/script.js
+++ 09-weatherJS/script.js
@@ -6,7 +6,22 @@ const ui = new UI();
 
 // get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
-// weather.changeLocation('London', 'GB');
+
+// change location event
+document.getElementById('w-change-btn').addEventListener('click', (e)=>{
+    const city = document.getElementById('city').value;
+    const country = document.getElementById('country').value;
+
+    weather.changeLocation(city, country);
+
+    // get and display weather
+    getWeather();
+
+    // close modal
+    $('#locModal').modal('hide');
+
+});
+
 
 function getWeather(){
     weather.getWeather()
